b36a2b3bc12fefff2c22d7f2a9b1bbda2a9b8130
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-07-31 19:15:05 +0100

    fixed currentUser props

--- API/Models/CurrentUser.cs
+++ API/Models/CurrentUser.cs
@@ -2,9 +2,10 @@ namespace API.Models
 {
     public class CurrentUser
     {
-        public int user_id { get; set; }
+        public string user_id { get; set; }
         public string username { get; set; }
         public string email { get; set; }
+        public string avatar { get; set; }
         public string token { get; set; }
     }
 }
