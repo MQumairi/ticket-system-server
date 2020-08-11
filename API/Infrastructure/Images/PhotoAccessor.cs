20ef2297a31842edacf74429891461fc95aa800d
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-08-11 13:57:35 +0100

    photo accessor added

--- API/Infrastructure/Images/PhotoAccessor.cs
+++ API/Infrastructure/Images/PhotoAccessor.cs
@@ -11,7 +11,7 @@ namespace API.Infrastructure.Images
 
         public PhotoAccessor(IOptions<CloudinarySettings> config)
         {
-            var acc = new Account (
+            var acc = new Account(
                 config.Value.cloudName,
                 config.Value.apiKey,
                 config.Value.apiSecret
@@ -20,25 +20,39 @@ namespace API.Infrastructure.Images
             this.cloudinary = new Cloudinary(acc);
         }
 
-        PhotoUploadResult AddPhoto(IFormFile file) {
+        public PhotoUploadResult AddPhoto(IFormFile file)
+        {
 
             var uploadResult = new ImageUploadResult();
-            
-            if(file.Length > 0) {
-                using(var stream = file.OpenReadStream()) { 
 
-                    var uploadParams = new ImageUploadParams{
+            if (file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+
+                    var uploadParams = new ImageUploadParams
+                    {
                         File = new FileDescription(file.FileName, stream)
                     };
 
-                    uploadResult = cloudinary.Upload(uploadParams);
+                    uploadResult = this.cloudinary.Upload(uploadParams);
                 }
             }
 
-            return new PhotoUploadResult{
+            return new PhotoUploadResult
+            {
                 PublicId = uploadResult.PublicId,
                 Url = uploadResult.SecureUrl.AbsoluteUri
             };
+        }
+
+        public string DeletePhoto(string publicId)
+        {
+
+            var deleteParams = new DeletionParams(publicId);
+            var result = this.cloudinary.Destroy(deleteParams);
+
+            return result.Result;
         }
     }
 }
